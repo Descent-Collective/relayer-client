44c8b14272fb29582bbede3fa188e15e8155bcd3
Michael Amadi <amadimichaeld@gmail.com>
2023-10-13 10:18:56 +0100

    update to match median abi and test with local node

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
+use core::panic;
 use dotenv::dotenv;
 use ethers::{
-    abi::{encode, Token},
+    abi::{encode, FixedBytes, Token},
     prelude::abigen,
     providers::{Http, Provider},
     signers::{LocalWallet, Signer},
-    types::{Address, H256, U256},
-    utils::{parse_units, ParseUnits},
+    types::{Address, Bytes, H256, U256},
+    utils::{hex::FromHex, parse_units, ParseUnits},
 };
 use std::{str::FromStr, sync::Arc};
 
@@ -24,13 +25,11 @@ pub async fn run() -> eyre::Result<()> {
 
     // define vectors to be used to store variables to parse to the function onchain
     let mut prices: Vec<U256> = Vec::new();
-    let mut timestamps: Vec<U256> = Vec::new();
-    let mut r: Vec<[u8; 32]> = Vec::new();
-    let mut s: Vec<[u8; 32]> = Vec::new();
-    let mut v: Vec<u8> = Vec::new();
+    let mut timestamps: Vec<u64> = Vec::new();
+    let mut signatures: Vec<Bytes> = Vec::new();
 
     // filter the price and timestamp info and push to their respective vectors
-    for (p, t) in prices_and_timestamps.iter() {
+    for (p, t) in prices_and_timestamps.into_iter() {
         // turn into a 6 fixed-point unsigned integer
         let first = parse_units(p, 6).expect("failed to convert float to ethers unit");
 
@@ -42,20 +41,20 @@ pub async fn run() -> eyre::Result<()> {
 
         // push to respective arrays
         prices.push(second);
-        timestamps.push(U256::try_from(*t).expect("could not convert from u64 to timestamp"));
+        timestamps.push(t);
     }
 
     // define interface for update fn call
     abigen!(
         IOracleModule,
         r#"[
-            function update(uint256[] calldata prices, uint256[] calldata timestamps, bytes32[] calldata r, bytes32[] calldata s, uint8[] calldata v) external
+            function update(uint256[] calldata _prices, uint64[] calldata _timestamps, bytes[] calldata _signatures) external
         ]"#
     );
 
     // define rpc url and oracle module address
     let rpc_url: String = std::env::var("RPC_URL").expect("RPC_URL must be set in your .env file");
-    let oracle_module_address: Address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".parse()?; // shouldn't revert
+    let oracle_module_address: Address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512".parse()?; // shouldn't revert
 
     // define provider to use from rpc url
     let provider = Provider::<Http>::try_from(rpc_url)?;
@@ -67,7 +66,7 @@ pub async fn run() -> eyre::Result<()> {
     // wallet to sign tx. get private key from env, and revert if any unexpected stuff happens
     let wallet = LocalWallet::from_bytes(
         H256::from_str(
-            &std::env::var("PRIVATE_KEY").expect("RPC_URL must be set in your .env file"),
+            &std::env::var("PRIVATE_KEY").expect("PRIVATE_KEY must be set in your .env file"),
         )
         .expect("invalid hex private key")
         .as_bytes(),
@@ -77,29 +76,37 @@ pub async fn run() -> eyre::Result<()> {
     // loop through price and timestamp array, abi encode them concatenated together, hash and sign it as "\x19Ethereum Signed Message:\n" + len + encoded message
     for i in 0..prices.len() {
         // encode price + timestamp. where + is concatenation
-        let message = encode(&[Token::Uint(prices[i]), Token::Uint(timestamps[i])]);
+        let message = encode(&[
+            Token::Uint(prices[i]),
+            Token::Uint(
+                U256::try_from(timestamps[i]).expect("could not convert from u64 to timestamp"),
+            ),
+            Token::FixedBytes(FixedBytes::from(
+                Vec::from_hex("555344432f784e474e0000000000000000000000000000000000000000000000")?, // hex of "USDC/xNGN"
+            )),
+        ]);
 
         // sign message with wallet
         let signature = wallet.sign_message(&message).await?;
 
         // push to vector
-        r.push(u64_array_to_u8_array(signature.r.0));
-        s.push(u64_array_to_u8_array(signature.s.0));
-        v.push(signature.v as u8);
+        let signature = Bytes::from(signature.to_vec());
+        if signature.len() != 65 {
+            panic!("invalid sig length");
+        }
+        signatures.push(signature);
     }
 
     // print values
     println!("addr: {:?}", wallet.address());
     println!("prices: {:?}", prices);
     println!("time: {:?}", timestamps);
-    println!("r: {:?}", r);
-    println!("s: {:?}", s);
-    println!("v: {:?}", v);
+    println!("_signatures: {:?}", signatures);
 
     // Update onchain oracle
     // Uncomment this, run `anvil` in your terminal, set env rpc url to 127.0.0.1:8545 and run `cargo run`
     // let built_tx_object = _oracle_module
-    //     .update(prices, timestamps, r, s, v)
+    //     .update(prices, timestamps, signatures)
     //     .from(wallet.address());
     // let tx = built_tx_object.send().await?;
     // println!("{:?}", built_tx_object);
